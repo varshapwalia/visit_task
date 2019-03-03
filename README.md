cdc3e496d06cc20abbf570fa6e200f8c553d2f6b
Varshap Walia <varshap@inclov.com>
2019-03-03 12:53:11 +0530

    creating DB and fetching users

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ visit_task contains all url endpoints for client side application
 
 1. Clone the whole project
 2. Install the required packages
-3. Run with nodemon app.js
+3. Run with `nodemon app.js`
 
 ## Structure
 
@@ -17,9 +17,9 @@ visit_task contains all url endpoints for client side application
 2. public folder will hold the templates/html
 3. Routers folder will route url/uris to the api.
 4. scripts are for internal and local cron jobs
-5. app.js will run the project with assigned port.
-6. Procfile is used for hosting it on Heroku to give it an environment.
-7. Package files contains the required packages
+5. `app.js` will run the project with assigned port.
+6. `Procfile` is used for hosting it on Heroku to give it an environment.
+7. `package.json` files contains the required packages
 
 ## Ways to improve
 
--- routers/user.js
+++ routers/user.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
+    host: '127.0.0.1',
     user: 'root',
     password: 'password',
     database: 'visit'
@@ -14,5 +14,59 @@ const pool = mysql.createPool({
 function getConnection() {
     return pool
 }
+
+router.get("/users", (req, res) => {
+    const connection = getConnection()
+    const queryString = "SELECT * FROM USERDATA"
+    connection.query(queryString, (err, rows, fields) => {
+      if (err) {
+        console.log("Failed to query for users: " + err)
+        res.sendStatus(500)
+        return
+      }
+      res.json(rows)
+    })
+  })
+
+
+// router.post('/user_create', (req, res) => {
+//     console.log("Trying to create a new user...")
+//
+//     console.log("First name: " + req.body.full_name)
+//     const user_id = req.body.user_id
+//     const user_name = req.body.user_name
+//     const dates = req.body.dates
+//     const steps = req.body.steps
+//     const calories = req.body.calories
+//
+//     const queryString = "INSERT INTO USERDATA (user_id, user_name, dates, steps, calories) VALUES (?, ?, ?, ?, ?)"
+//     getConnection().query(queryString, [user_id, user_name, dates, steps, calories], (err, results, fields) => {
+//       if (err) {
+//         console.log("Failed to insert new user: " + err)
+//         res.sendStatus(500)
+//         return
+//       }
+//
+//       console.log("Inserted a new user with id: ", results.insertId);
+//       res.end()
+//     })
+//   })
+
+// router.get("/create_table", (req, res) => {
+//   const connection = getConnection()
+//   // if (err) throw err;
+//   console.log("Connected!");
+//   var sql = "CREATE TABLE USERDATA (user_id INT, user_name VARCHAR(255), dates Date, steps INT, calories INT)";
+//   connection.query(sql, function (err, result) {
+//     // if (err) throw err;
+//     console.log("Table created");
+//     if (err) {
+//       console.log("Failed to query for users: " + err)
+//       res.sendStatus(500)
+//       return
+//     }
+//   });
+//   res.end()
+// });
 
 module.exports = router
